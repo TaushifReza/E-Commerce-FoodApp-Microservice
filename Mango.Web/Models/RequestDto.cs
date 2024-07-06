00caa6af771b927110a8fa3c9ffe3319748c330a
MOHAMMAD Taushif Reza <112973122+TaushifReza@users.noreply.github.com>
2024-07-06 18:42:23 +0545

    Create Request and Response Dto for Frontend

---
+++ Mango.Web/Models/RequestDto.cs
@@ -0,0 +1,12 @@
+﻿using Mango.Web.Utility;
+
+namespace Mango.Web.Models
+{
+    public class RequestDto
+    {
+        public SD.ApiType ApiType { get; set; } = SD.ApiType.GET;
+        public string ApiUrl { get; set; }
+        public object Data { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
