30ed967e957ab7625f703218dfe95b3dd6a8ff32
MOHAMMAD Taushif Reza <112973122+TaushifReza@users.noreply.github.com>
2024-07-06 18:30:18 +0545

    Complete CRUD API for CouponAPI and Create Frontent MCV Project

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -2,7 +2,6 @@
 using Mango.Services.CouponAPI.Data;
 using Mango.Services.CouponAPI.Models;
 using Mango.Services.CouponAPI.Models.Dto;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.CouponAPI.Controllers
@@ -47,6 +46,83 @@ namespace Mango.Services.CouponAPI.Controllers
             {
                 var coupon = _db.Coupons.FirstOrDefault(c => c.CouponId == id);
                 _response.Result = _mapper.Map<CouponDto>(coupon); ;
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.Message = e.Message;
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("GetByCode/{code}")]
+        public ResponseDto Get(string code)
+        {
+            try
+            {
+                var coupon = _db.Coupons.FirstOrDefault(c => c.CouponCode.ToLower() == code.ToLower());
+                if (coupon == null) {
+                    _response.IsSuccess = false;
+                    return _response;
+                }
+                _response.Result = _mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.Message = e.Message;
+            }
+            return _response;
+        }
+
+        [HttpPost]
+        public ResponseDto Post([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                var coupon = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Add(coupon);
+                _db.SaveChanges();
+                
+                _response.Result = _mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.Message = e.Message;
+            }
+            return _response;
+        }
+
+        [HttpPut]
+        public ResponseDto Put([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                var coupon = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Update(coupon);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.Message = e.Message;
+            }
+            return _response;
+        }
+
+        [HttpDelete]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+
+                var coupon = _db.Coupons.FirstOrDefault(c => c.CouponId == id);
+                _db.Coupons.Remove(coupon);
+                _db.SaveChanges();
             }
             catch (Exception e)
             {
--- Mango.Services.CouponAPI/Models/Dto/CouponDto.cs
+++ Mango.Services.CouponAPI/Models/Dto/CouponDto.cs
@@ -4,7 +4,7 @@
     {
         public int CouponId { get; set; }
         public string CouponCode { get; set; }
-        public string DiscountAmount { get; set; }
+        public double DiscountAmount { get; set; }
         public int MinAmount { get; set; }
     }
 }
