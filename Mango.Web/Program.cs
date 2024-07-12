7715fa9a577e5e91409758cdfdde7b976f02e100
MOHAMMAD Taushif Reza <112973122+TaushifReza@users.noreply.github.com>
2024-07-12 23:56:53 +0545

    Add Dto and Service Classes for AuthAPI in Frontend

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -9,11 +9,14 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient();
 builder.Services.AddHttpClient<ICouponService, CouponService>();
+builder.Services.AddHttpClient<IAuthService, AuthService>();
 
 SD.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"];
+SD.AuthAPIBase = builder.Configuration["ServiceUrls:AuthAPI"];
 
 builder.Services.AddScoped<IBaseService, BaseService>();
 builder.Services.AddScoped<ICouponService, CouponService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 var app = builder.Build();
 
