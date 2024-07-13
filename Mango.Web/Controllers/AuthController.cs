a51f7e2b833c6e094643dcf38db62ee7d05d4bb9
MOHAMMAD Taushif Reza <112973122+TaushifReza@users.noreply.github.com>
2024-07-13 22:50:32 +0545

    Complete API integration in Auth for Frontend

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using Mango.Web.Service.IService;
 using Mango.Web.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 
 namespace Mango.Web.Controllers
 {
@@ -22,6 +23,22 @@ namespace Mango.Web.Controllers
             return View(loginRequestDto);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequestDto obj)
+        {
+            var response = await _authService.LoginAsync(obj);
+            if (response != null && response.IsSuccess)
+            {
+                var loginResponseDto =
+                    JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(response.Result));
+                return RedirectToAction("Index", "Home");
+            }else
+            {
+                ModelState.AddModelError("CustomerError", response.Message);
+                return View(obj);
+            }
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
@@ -32,6 +49,35 @@ namespace Mango.Web.Controllers
             };
             ViewBag.RoleList = roleList;
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationRequestDto obj)
+        {
+            ResponseDto result = await _authService.RegisterAsync(obj);
+            ResponseDto assignRole;
+            if (result != null && result.IsSuccess)
+            {
+                if (string.IsNullOrEmpty(obj.Role))
+                {
+                    obj.Role = SD.RoleCustomer;
+                }
+
+                assignRole = await _authService.AssignRoleAsync(obj);
+                if (assignRole != null && assignRole.IsSuccess)
+                {
+                    TempData["Success"] = "Registration Successful";
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem{Text = SD.RoleAdmin, Value = SD.RoleAdmin},
+                new SelectListItem{Text = SD.RoleAdmin, Value = SD.RoleCustomer},
+            };
+            ViewBag.RoleList = roleList;
+            return View(obj);
         }
 
         public IActionResult Logout()
