d0c18bf8845ca398d619451df7d9d1a4b35f31ea
MOHAMMAD Taushif Reza <112973122+TaushifReza@users.noreply.github.com>
2024-09-14 19:07:07 +0545

    Create new API project for ShoopingCart and create model and dto

---
+++ Mango.Services.ShoppingCartAPI/Models/Dto/CartHeaderDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Mango.Services.ShoppingCartAPI.Models.Dto
+{
+    public class CartHeaderDto
+    {
+        public int CartHeaderId { get; set; }
+        public string? UserId { get; set; }
+        public string? CouponId { get; set; }
+
+        public double Discount { get; set; }
+        public double CartTotal { get; set; }
+    }
+}
