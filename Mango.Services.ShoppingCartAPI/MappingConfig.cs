35a7083d8166be17d5e39d0f730814f8617d5546
MOHAMMAD Taushif Reza <112973122+TaushifReza@users.noreply.github.com>
2024-09-14 20:00:54 +0545

    Create an API for Adding Updating product in cart

---
+++ Mango.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Mango.Services.ShoppingCartAPI
+{
+    public class MappingConfig : Profile
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+                config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+            });
+            return mappingConfig;
+        }
+    }
+}
